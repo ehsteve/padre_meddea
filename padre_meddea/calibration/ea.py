02e2c6d84ec44b98dc7c74e21e7905b30b61709b
Steven Christe <steven.christe@nasa.gov>
2026-04-10 13:58:32 -0400

    decimation info

---
+++ padre_meddea/calibration/ea.py
@@ -0,0 +1,50 @@
+""""""
+
+import astropy.units as u
+from roentgen.absorption import Material
+
+
+def get_effective_area(energy: u.keV, no_al=False, no_be=False, no_mli=False):
+    """Returns the model effective area
+
+    Parameters
+    ----------
+    energy : u.keV
+    no_al : bool, optional
+        If True, ignore the aluminum layer. Default is False.
+    no_be : bool, optional
+        If True, ignore the beryllium layer. Default is False.
+    no_mli : bool, optional
+        If True, ignore the MLI blankets. Default is False.
+    Returns
+    -------
+    effective_area : u.cm^2
+    """
+
+    al_thickness = 0.4 * u.mm
+    be_thickness = 1 * u.mm
+    number_of_detectors = 4
+    # caliste so properties see https://github.com/i4Ds/STIXCore/blob/master/stixcore/calibration/transmission.py#L18
+    geo_area_detector = 0.81 * u.cm**2
+    detector_thickness = 1 * u.mm
+    meddea_geo_area = number_of_detectors * geo_area_detector
+
+    det_cathode = Material("Pt", 30 * u.nm)
+    detector_efficiency = Material("cdte", thickness=detector_thickness).absorption(
+        energy
+    ) * det_cathode.transmission(energy)
+    # MLI blankets
+    MIL_SI = 0.0254 * u.mm
+
+    blankets = Material("C", (3 + 20 * 0.05 + 3) * MIL_SI)
+
+    detector_area = meddea_geo_area * detector_efficiency
+    effective_area = detector_area
+
+    if not no_mli:
+        effective_area *= blankets.transmission(energy)
+    if not no_al:
+        effective_area *= Material("Al", thickness=al_thickness).transmission(energy)
+    if not no_be:
+        effective_area *= Material("Be", thickness=be_thickness).transmission(energy)
+    return effective_area
